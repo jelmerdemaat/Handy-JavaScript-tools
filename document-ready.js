d96c2ae962416e2c6053961a4b3c83fec8b3dfd1
Jelmer de Maat <post@jelmerdemaat.nl>
2014-09-03 15:41:46 +0200

    added DOM ready check

---
+++ document-ready.js
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////
+// Checking for if the document/DOM is ready
+///////////////////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	// Your code here.
+
+});
